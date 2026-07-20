c9fe53ccd001a875785ddfbfbd5e1ca643f5ea3e
justclaner <justclaner@gmail.com>
2026-07-20 17:23:05 -0400

    added route to change user password and unit testing for users route

--- api/routes/users.js
+++ api/routes/users.js
@@ -17,26 +17,6 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// Make a user
-router.post("/", async (req, res, next) => {
-  try {
-    const { email, password } = req.body || {};
-    if (email == undefined) {
-      return res.status(400).json({ error: "email is required!" });
-    }
-    if (password == undefined) {
-      return res.status(400).json({ error: "password is required!" });
-    }
-    const hashedPassword = await hashString(password, 12);
-    const insertCommand = await db
-      .prepare(`INSERT INTO users (email, password_hash) VALUES (?, ?)`)
-      .run(email, hashedPassword);
-    return res.status(201).json({ data: insertCommand });
-  } catch (e) {
-    next(e);
-  }
-});
-
 // Get a specific user
 router.get("/:userId", async (req, res, next) => {
   try {
@@ -50,6 +30,70 @@ router.get("/:userId", async (req, res, next) => {
         .json({ error: `User with id ${userId} does not exist!` });
     }
     return res.status(200).json({ data: user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Make a user
+router.post("/", async (req, res, next) => {
+  try {
+    const { email, password } = req.body || {};
+    if (email == undefined) {
+      return res.status(400).json({ error: "email is required!" });
+    }
+    const user = await db
+      .prepare(`SELECT * FROM users WHERE email = ?`)
+      .get(email);
+    if (user) {
+      return res.status(400).json({
+        error: `User with email ${email} already exists!`,
+      });
+    }
+
+    if (password == undefined) {
+      return res.status(400).json({ error: "password is required!" });
+    }
+    const hashedPassword = await hashString(password, 12);
+    const insertCommand = await db
+      .prepare(`INSERT INTO users (email, password_hash) VALUES (?, ?)`)
+      .run(email, hashedPassword);
+    return res.status(201).json({ data: insertCommand });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Change password
+router.patch("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const { password } = req.body || {};
+
+    const user = await db
+      .prepare(`SELECT * FROM users WHERE id = ?`)
+      .get(userId);
+    if (user == undefined) {
+      return res
+        .status(404)
+        .json({ error: `User with id ${userId} does not exist!` });
+    }
+
+    if (password == undefined) {
+      return res.status(400).json({ error: "password is required!" });
+    }
+    const hashedPassword = await hashString(password, 12);
+    const updateCommand = await db
+      .prepare(
+        `
+        UPDATE users 
+        SET password_hash = ?
+        WHERE id = ?
+      `,
+      )
+      .run(hashedPassword, userId);
+
+    return res.status(200).json({ data: updateCommand });
   } catch (e) {
     next(e);
   }
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import request from "supertest";
 import app from "../api/app.js";
+import { createTestUser } from "./helpers.js";
 
 describe("POST /api/users", () => {
   it("creates a user with valid email and password", async () => {
@@ -28,6 +29,51 @@ describe("POST /api/users", () => {
 
     expect(res.status).toBe(400);
     expect(res.body.error).toMatch(/password/i);
+  });
+
+  it("creating user with taken email returns 400", async () => {
+    const res1 = await request(app)
+      .post("/api/users")
+      .send({ email: "test@example.com", password: "password123" });
+    expect(res1.status).toBe(201);
+
+    const res2 = await request(app)
+      .post("/api/users")
+      .send({ email: "test@example.com", password: "password123" });
+    expect(res2.status).toBe(400);
+  });
+});
+
+describe("PATCH /api/users", () => {
+  it("user not existing returns 404", async () => {
+    const res = await request(app).patch("/api/users/1").send({});
+    expect(res.status).toBe(404);
+  });
+
+  it("no password returns 400", async () => {
+    const userId = await createTestUser();
+    const res = await request(app).patch(`/api/users/${userId}`).send({});
+    expect(res.status).toBe(400);
+  });
+
+  it("password is successfully updated", async () => {
+    const userId = await createTestUser();
+    const res1 = await request(app).patch(`/api/users/${userId}`).send({
+      password: "abc1234",
+    });
+    expect(res1.status).toBe(200);
+
+    const user = await request(app).get(`/api/users/${userId}`);
+    expect(user.status).toBe(200);
+    const email = user.body.data.email;
+
+    const res2 = await request(app).post("/api/users/auth").send({
+      email: email,
+      password: "abc1234",
+    });
+    console.log(res2);
+    expect(res2.status).toBe(200);
+    expect(res2.body.data.success).toBe(true);
   });
 });
 
